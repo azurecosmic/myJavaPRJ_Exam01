b6567d5ca2cb3efb54e6f6b0b6391acf53d5c99e
azurecosmic <kyurinkim1106@gmail.com>
2022-04-01 10:05:31 +0900

    "2022.04.01 commit"

---
+++ src/main/java/Ex03_01.java
@@ -0,0 +1,10 @@
+public class Ex03_01 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.printf("\n");
+        System.out.printf("%d", 100+100);
+        System.out.printf("\n");
+    }
+}
+
+// 큰따옴표 사용시 문자/숫자일경우 %d를 붙여서 사용 => 숫자에는 따옴표 사용 x
