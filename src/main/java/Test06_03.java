dc38ba228683f3e9fce6395d9cec88d0bd25c44d
azurecosmic <kyurinkim1106@gmail.com>
2022-04-26 17:12:49 +0900

    "2022.04.26 parseInt commit"

---
+++ src/main/java/Test06_03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test06_03 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        String  a = sc.nextLine();
+
+        int num = Integer.parseInt(a,16);
+
+        for(int i=1; i<16; i++) {
+
+            System.out.format("%X*%X=%X%n",num,i,num*i);
+        }
+    }
+}
