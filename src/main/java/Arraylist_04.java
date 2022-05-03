275138330b45b9b48b1bfdc5881f82e573df735d
azurecosmic <kyurinkim1106@gmail.com>
2022-05-03 22:39:37 +0900

    "2022.05.03 ArrayList commit"

--- src/main/java/Arraylist_04.java
+++ src/main/java/Arraylist_04.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Arraylist_02 {
+public class Arraylist_04 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
 
