4a2ac794e165ac63e5d6d41e943ea5fcf31714c4
azurecosmic <kyurinkim1106@gmail.com>
2022-05-24 21:19:12 +0900

    "2022.05.23 문자열 commit"

---
+++ src/main/java/Self/String_01.java
@@ -0,0 +1,29 @@
+package Self;
+
+import java.util.Scanner;
+
+public class String_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String str;
+
+        System.out.print("문자열 입력 ==> ");
+        str = sc.nextLine();
+
+        System.out.print("출력 문자열 ==> ");
+
+        if (!str.startsWith("("))
+            System.out.print("(");
+
+        for(int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i));
+        }
+
+        if (!str.startsWith(")"))
+            System.out.print(")");
+
+        String log = "111.222.33.44 - sfbdfikfn";
+
+
+    }
+}
