4df745e5d1f01848d61f96902970a5a54c6a4a2a
azurecosmic <kyurinkim1106@gmail.com>
2022-06-03 09:08:48 +0900

    "2022.06.03 reverse 문제풀이 commit"

---
+++ src/main/java/Self/Reverse_01.java
@@ -0,0 +1,33 @@
+package Self;
+
+import java.util.Scanner;
+
+public class Reverse_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String reserve1 ="";
+        String reserve2 ="";
+
+        for (int i =a.length()-1; i >=0; i--) {
+            reserve1 += a.charAt(i);
+        }
+        for (int i =b.length()-1; i >=0 ; i--) {
+            reserve2 += b.charAt(i);
+        }
+        int a1 = Integer.parseInt(reserve1);
+        int a2 = Integer.parseInt(reserve2);
+
+        if (a1 > a2) {
+            System.out.println(a1);
+        }
+        if (a1 < a2) {
+            System.out.println(a2);
+        }
+
+    }
+
+}
