3b2c9179e83d9381caa9a6ff55f2bda97fcbff17
azurecosmic <kyurinkim1106@gmail.com>
2022-06-17 17:41:02 +0900

    "2022.06.18 commit"

---
+++ src/main/java/Max_01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Max_01 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int cnt = 1;
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+            if (max < arr[i]) {
+                max = arr[i];
+                cnt = i+1;
+            }
+
+
+
+        }
+        System.out.println(max);
+        System.out.print(cnt);
+    }
+}
